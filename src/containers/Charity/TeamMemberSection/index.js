0cc313b6fee21f9b332a3f8318419a46c0ec1951
sudhakarsonkar <sudhakarsonkar007@gmail.com>
2023-01-30 13:50:21 +0530

    team page added

---
+++ src/containers/Charity/TeamMemberSection/index.js
@@ -0,0 +1,190 @@
+import React from 'react';
+import '../../styles/teamMember.module.css'
+
+const TeamMemberSection = () => {
+  return (
+<>
+  <div className="container-block container">
+  <p className="text-blk team-head-text">Our Team</p>
+  <div className="responsive-container-block teamcard-container">
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft13.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft14.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft13.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft14.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft13.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft14.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft13.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+    <div className="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div className="team-card">
+        <div className="team-img-wrapper">
+          <img
+            className="team-img"
+            src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft14.svg"
+          />
+        </div>
+        <div className="team-card-content">
+          <p className="text-blk name">Roger Lubin</p>
+          <p className="text-blk position">CEO</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </>
+  );
+};
+
+
+
+
+export default TeamMemberSection;
+
+
---
+++ src/containers/Charity/TeamMemberSection/teamMember.style.js
@@ -0,0 +1,156 @@
+const style = {
+  "*": { fontFamily: "Nunito, sans-serif" },
+  ".text-blk": {
+    marginTop: "0px",
+    marginRight: "0px",
+    marginBottom: "0px",
+    marginLeft: "0px",
+    lineHeight: "25px"
+  },
+  ".responsive-container-block": {
+    minHeight: "75px",
+    height: "fit-content",
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "0px",
+    marginRight: "auto",
+    marginBottom: "0px",
+    marginLeft: "auto",
+    justifyContent: "flex-start"
+  },
+  ".container-block": {
+    minHeight: "75px",
+    height: "fit-content",
+    width: "100%",
+    paddingTop: "10px",
+    paddingRight: "10px",
+    paddingBottom: "10px",
+    paddingLeft: "10px",
+    display: "block"
+  },
+  ".responsive-cell-block": { minHeight: "75px" },
+  ".container": {
+    maxWidth: "1380px",
+    marginTop: "80px",
+    marginRight: "auto",
+    marginBottom: "80px",
+    marginLeft: "auto",
+    paddingTop: "0px",
+    paddingRight: "30px",
+    paddingBottom: "0px",
+    paddingLeft: "30px"
+  },
+  ".team-head-text": {
+    fontSize: "48px",
+    lineHeight: "50px",
+    fontWeight: 900,
+    textAlign: "center",
+    marginTop: "0px",
+    marginRight: "0px",
+    marginBottom: "70px",
+    marginLeft: "0px"
+  },
+  ".name": {
+    fontSize: "18px",
+    marginTop: "11px",
+    marginRight: "0px",
+    marginBottom: "0px",
+    marginLeft: "0px",
+    fontWeight: 700
+  },
+  ".position": {
+    marginTop: "5px",
+    marginRight: "0px",
+    marginBottom: "0px",
+    marginLeft: "0px"
+  },
+  ".team-card-container": {
+    width: "263px",
+    marginTop: "0px",
+    marginRight: "15px",
+    marginBottom: "30px",
+    marginLeft: "15px"
+  },
+  ".teamcard-container": { justifyContent: "space-evenly" },
+  ".team-img-wrapper": {
+    borderTopLeftRadius: "6px",
+    borderTopRightRadius: "6px",
+    borderBottomRightRadius: "6px",
+    borderBottomLeftRadius: "6px"
+  },
+  ".team-img": { maxWidth: "100%" },
+  "@media (max-width: 500px)": [
+    {
+      ".team-card-container": {
+        marginTop: "0px",
+        marginRight: "0px",
+        marginBottom: "30px",
+        marginLeft: "0px",
+        width: "100%"
+      },
+      ".team-img": { width: "100%" }
+    },
+    {
+      ".wk-mobile-1": { width: "8.333333%" },
+      ".wk-mobile-2": { width: "16.666667%" },
+      ".wk-mobile-3": { width: "25%" },
+      ".wk-mobile-4": { width: "33.333333%" },
+      ".wk-mobile-5": { width: "41.666667%" },
+      ".wk-mobile-6": { width: "50%" },
+      ".wk-mobile-7": { width: "58.333333%" },
+      ".wk-mobile-8": { width: "66.666667%" },
+      ".wk-mobile-9": { width: "75%" },
+      ".wk-mobile-10": { width: "83.333333%" },
+      ".wk-mobile-11": { width: "91.666667%" },
+      ".wk-mobile-12": { width: "100%" }
+    }
+  ],
+  "@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800&amp;display=swap')": true,
+  "*,\n*:before,\n*:after": {
+    MozBoxSizing: "border-box",
+    WebkitBoxSizing: "border-box",
+    boxSizing: "border-box"
+  },
+  body: { margin: "0" },
+  ".wk-desk-1": { width: "8.333333%" },
+  ".wk-desk-2": { width: "16.666667%" },
+  ".wk-desk-3": { width: "25%" },
+  ".wk-desk-4": { width: "33.333333%" },
+  ".wk-desk-5": { width: "41.666667%" },
+  ".wk-desk-6": { width: "50%" },
+  ".wk-desk-7": { width: "58.333333%" },
+  ".wk-desk-8": { width: "66.666667%" },
+  ".wk-desk-9": { width: "75%" },
+  ".wk-desk-10": { width: "83.333333%" },
+  ".wk-desk-11": { width: "91.666667%" },
+  ".wk-desk-12": { width: "100%" },
+  "@media (max-width: 1024px)": {
+    ".wk-ipadp-1": { width: "8.333333%" },
+    ".wk-ipadp-2": { width: "16.666667%" },
+    ".wk-ipadp-3": { width: "25%" },
+    ".wk-ipadp-4": { width: "33.333333%" },
+    ".wk-ipadp-5": { width: "41.666667%" },
+    ".wk-ipadp-6": { width: "50%" },
+    ".wk-ipadp-7": { width: "58.333333%" },
+    ".wk-ipadp-8": { width: "66.666667%" },
+    ".wk-ipadp-9": { width: "75%" },
+    ".wk-ipadp-10": { width: "83.333333%" },
+    ".wk-ipadp-11": { width: "91.666667%" },
+    ".wk-ipadp-12": { width: "100%" }
+  },
+  "@media (max-width: 768px)": {
+    ".wk-tab-1": { width: "8.333333%" },
+    ".wk-tab-2": { width: "16.666667%" },
+    ".wk-tab-3": { width: "25%" },
+    ".wk-tab-4": { width: "33.333333%" },
+    ".wk-tab-5": { width: "41.666667%" },
+    ".wk-tab-6": { width: "50%" },
+    ".wk-tab-7": { width: "58.333333%" },
+    ".wk-tab-8": { width: "66.666667%" },
+    ".wk-tab-9": { width: "75%" },
+    ".wk-tab-10": { width: "83.333333%" },
+    ".wk-tab-11": { width: "91.666667%" },
+    ".wk-tab-12": { width: "100%" }
+  }
+}
